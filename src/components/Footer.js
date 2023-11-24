f03f3448a4b4277dfe1ca1716ddb176f86081ae3
Natan-Mesele <huluale12@gmail.com>
2023-11-24 18:44:33 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -104,32 +104,7 @@ const Footer = () => {
           <Youtube style={{ fontSize: '2.5rem' }} />
           <Wifi style={{ fontSize: '2.5rem' }} />
         </Container>
-        <Footer>
-            <Footer.Wrapper>
-            <Footer.Row>
-                <Footer.Column>
-                <Footer.Title>About Us</Footer.Title>
-                    <Footer.Link href="#">Story</Footer.Link>
-                    <Footer.Link href="#">Clients</Footer.Link>
-                    <Footer.Link href="#">Testimonials</Footer.Link>
-                </Footer.Column>
-                <Footer.Column>
-                <Footer.Title>Services</Footer.Title>
-                    <Footer.Link href="#">Marketing</Footer.Link>
-                    <Footer.Link href="#">Consulting</Footer.Link>
-                    <Footer.Link href="#">Development</Footer.Link>
-                    <Footer.Link href="#">Design</Footer.Link>
-                </Footer.Column>
-                <Footer.Column>
-                <Footer.Title>Contact Us</Footer.Title>
-                    <Footer.Link href="#">United States</Footer.Link>
-                    <Footer.Link href="#">United Kingdom</Footer.Link>
-                    <Footer.Link href="#">Australia</Footer.Link>
-                    <Footer.Link href="#">Support</Footer.Link>
-                </Footer.Column>
-            </Footer.Row>
-            </Footer.Wrapper>
-        </Footer>
+        
       </main>
     </React.Fragment>
   );
@@ -159,57 +134,3 @@ const LinkedIn = styled(LinkedInIcon)`
 `
 const Wifi = styled(RssFeedIcon)`
 `
-export const Footers = styled.div`
-  padding: 80px 60px;
-  background: radial-gradient(circle, rgba(92,39,251,1) 0%, rgba(112,71,247,1) 100%);
-
-  @media (max-width: 1000px) {
-    padding: 70px 30px;
-  }
-`;
-
-export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 1000px;
-    margin: 0 auto;
-    /* background: red; */
-`
-
-export const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-left: 60px;
-`;
-
-export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-  grid-gap: 20px;
-
-  @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  }
-`;
-
-export const Link = styled.a`
-  color: #fff;
-  margin-bottom: 20px;
-  font-size: 18px;
-  text-decoration: none;
-
-  &:hover {
-      color: #ff9c00;
-      transition: 200ms ease-in;
-  }
-`;
-
-export const Title = styled.p`
-  font-size: 24px;
-  color: #fff;
-  margin-bottom: 40px;
-  font-weight: bold;
-`;
-
